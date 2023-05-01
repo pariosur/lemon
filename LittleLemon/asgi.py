44a338cc06ad3807e8849c4685d00371a092e27d
Pablo Rios <pariosur@gmail.com>
2023-05-01 17:27:13 +0200

    setting up database

--- LittleLemon/asgi.py
+++ LittleLemon/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for capstone project.
+ASGI config for LittleLemon project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'capstone.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LittleLemon.settings')
 
 application = get_asgi_application()
